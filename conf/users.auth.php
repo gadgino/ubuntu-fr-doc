b9fea6dbf7859458456d35c0bcba07a6a0ee30d8
youssoufB <youssouf.bologna@lilo.org>
2018-04-28 02:38:07 +0200

    ajout de fichier 1er téléversement

---
+++ conf/users.auth.php
@@ -0,0 +1,13 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+youssouf:$1$cP9GXEtJ$CsFlibN0LRB1wGNmi.d2y.:bologna:youssouf.bologna@lilo.org:admin,user
+youssoufuser:$1$tA3AXKD5$JTUAKHOBbeLX7bBD8Uv9A0:bologna:youssouf.bologna@lilo.org:user
